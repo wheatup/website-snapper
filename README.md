c068987b686ce4aa7358bb6b23c12a555d1774f3
wheatup <acphun@gmail.com>
2021-05-14 09:45:45 +0900

    update README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Website Snapper
 
-Website Snapper allowes you to take a snapshot of any website and show the result directly in your terminal!
+Website Snapper allowes you to take a snapshot of any website and ~~*kind of*~~ show the result directly inside of your terminal!
 
 ![demo](https://github.com/wheatup/website-snapper/blob/master/assets/demo.png?raw=true)
 ## Install
--- bin/index.js
+++ bin/index.js
@@ -17,41 +17,41 @@ const logBox = text => {
 };
 
 const snapURL = url => {
-	if(!url?.trim()) {
+	if (!url?.trim()) {
 		console.error('Please enter a URL!');
 		process.exit();
-		return;
-	}
-	url = url.replace(/^(?!\w+:\/\/)/, 'https://').replace(/^(?!.*\.)(.*)$/, '$1.com');
-	logBox(chalk.white('Opening ') + chalk.green(url));
+	} else {
+		url = url.replace(/^(?!\w+:\/\/)/, 'https://').replace(/^(?!.*\.)(.*)$/, '$1.com');
+		logBox(chalk.white('Opening ') + chalk.green(url));
 
-	(async () => {
-		let browser;
-		try {
-			browser = await puppeteer.launch({
-				defaultViewport: {
-					width: Math.max(~~(process.stdout.columns * 8), 800),
-					height: Math.max(~~(process.stdout.columns * 8 * (9 / 16)), 450)
-				}
-			});
-			const page = await browser.newPage();
-			const fileName = `__temp__${~~(Math.random() * 1e8)}.png`
-			await page.goto(url);
-			const dimensions = await page.evaluate(() => ({
-				width: document.documentElement.clientWidth,
-				height: document.documentElement.clientHeight,
-				deviceScaleFactor: window.devicePixelRatio,
-			}));
-			await page.screenshot({ path: fileName, width: dimensions.width, height: dimensions.height });
-			console.log(await terminalImage.file(fileName));
-			fs.unlink(fileName, () => { });
-		} catch (ex) {
-			console.log('Error: ' + ex.message);
-		} finally {
-			await browser?.close();
-			process.exit();
-		}
-	})();
+		(async () => {
+			let browser;
+			try {
+				browser = await puppeteer.launch({
+					defaultViewport: {
+						width: Math.max(~~(process.stdout.columns * 8), 800),
+						height: Math.max(~~(process.stdout.columns * 8 * (9 / 16)), 450)
+					}
+				});
+				const page = await browser.newPage();
+				const fileName = `__temp__${~~(Math.random() * 1e8)}.png`
+				await page.goto(url);
+				const dimensions = await page.evaluate(() => ({
+					width: document.documentElement.clientWidth,
+					height: document.documentElement.clientHeight,
+					deviceScaleFactor: window.devicePixelRatio,
+				}));
+				await page.screenshot({ path: fileName, width: dimensions.width, height: dimensions.height });
+				console.log(await terminalImage.file(fileName));
+				fs.unlink(fileName, () => { });
+			} catch (ex) {
+				console.log('Error: ' + ex.message);
+			} finally {
+				await browser?.close();
+				process.exit();
+			}
+		})();
+	}
 };
 
 const argv = hideBin(process.argv);
